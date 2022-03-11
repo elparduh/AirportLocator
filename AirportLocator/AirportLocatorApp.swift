f1542501a841b3428405a6b8f5d20bee9f650cbd
Juan Ticante <juan.ticante@albo.mx>
2022-03-11 09:11:56 -0600

    Initial Commit

---
+++ AirportLocator/AirportLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirportLocatorApp.swift
+//  AirportLocator
+//
+//  Created by Juan Ticante Vicente on 11/03/22.
+//
+
+import SwiftUI
+
+@main
+struct AirportLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
